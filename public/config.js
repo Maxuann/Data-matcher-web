0ed08ea09fda2b3e2de8350ec4ed6192a00a2594
Max <279603177@qq.com>
2020-06-25 22:41:07 +0800

    图标修改

---
+++ public/config.js
@@ -0,0 +1,17 @@
+GAME_DATA = [
+   {
+      id: 1029,
+      name: '消消乐',
+      iconAmount: 18
+   },
+   {
+      id: 1030,
+      name: '消消消乐',
+      iconAmount: 16
+   },
+   {
+      id: 1031,
+      name: '消消消消乐',
+      iconAmount: 14
+   }
+]
